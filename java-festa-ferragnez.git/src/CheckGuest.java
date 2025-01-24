813cf5427cdc12ff765a8f740dbe3f1a7c36c812
simonacorreale <simona.correale.work@gmail.com>
2025-01-24 14:58:05 +0100

    feat: Exercise comment

--- java-festa-ferragnez.git/src/CheckGuest.java
+++ java-festa-ferragnez.git/src/CheckGuest.java
@@ -1,7 +1,25 @@
+/* 
+
+State lavorando col servizio di sicurezza dei Ferragnez e il vostro compito è di assicurarvi che accedano alla festa solo gli invitati presenti sulla lista (in fondo al post)
+Consegna:
+Creare un progetto java-festa-ferragnez a cui aggiungere la classe CheckGuest
+Nel programma bisogna:
+- creare e inizializzare l’array contenente i nomi degli invitati
+- chiedere all’utente come si chiama
+- verificare che il nome sia presente nella lista
+- lasciarlo entrare o rispedirlo cortesemente da dove è venuto :sunglasses:
+Bonus
+se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso programma usando il ciclo while, e viceversa
+Buon lavoro e buon weekend! :slightly_smiling_face: :tada:
+Lista invitati:
+Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+ */
 public class CheckGuest{
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         
+        
+
     }
 }
